ccfe437ba6584373f80ac76c5547cabca12ac413
ThanhChi <thanhchi2007@gmail.com>
2020-09-18 20:15:36 +0700

    them xep loai yeu

--- models/SinhVien.js
+++ models/SinhVien.js
@@ -11,4 +11,7 @@ var SinhVien = function (masv,tensv,email,diemtoan,diemly,diemhoa,diemrenluyen,l
         var dtb = (Number(this.diemToan) + Number(this.diemLy) + Number(this.diemHoa)) /3;
         return dtb;
     }
+    this.xeploai=function(){
+        return 'yeu'
+    }
 }
